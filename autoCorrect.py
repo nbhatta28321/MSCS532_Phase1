4ddd7da3e0877e7235b85f909a6ebb6d4fbed48d
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2024-09-08 16:51:16 -0400

    search for prefix similarity

--- autoCorrect.py
+++ autoCorrect.py
@@ -1,1 +1,19 @@
-print("Auto correct")
+class AutoCorrect:
+    def __init__(self, words) -> None:
+        self.words = words
+        
+    def search(self, input) -> list[str]:
+        # print("words",words)
+        s = [w for w in self.words if w.startswith(input)]
+        if s:
+            return s
+        # for i in self.words:
+        #     if (i.startswith(input)):
+        #         return list[i]
+        return list["Empty"]
+
+
+words = ["apple", "app", "acid", "aerospace"]
+autocorrect = AutoCorrect(words)
+a = autocorrect.search("ap")
+print("Final", a)
