bb4270ed3298a33c246e2c2fa1ee870bb79dbbb5
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2024-09-22 12:33:46 -0400

    added modular format

--- autoCorrect.py
+++ autoCorrect.py
@@ -18,7 +18,8 @@ class AutoCorrect:
 
         for i in input: # check chracters deom the root nodes 
             if i not in cur.child: #return empty if not found
-                return []
+                return self.correction(input) #if the word is incorrect
+                # return []
             cur = cur.child[i] #point it to the child node that contains the character
 
         return self.collect_words(cur, input) 
@@ -32,3 +33,45 @@ class AutoCorrect:
             sugg.extend(self.collect_words(v, input + k))
 
         return sugg
+    
+    def biagam(self, word):
+        return [word[i] + word[i+1] for i in range(len(word)-1)]
+    
+    def similarity_ratio(self, a, b):
+        groupA, groupB = self.biagam(a), self.biagam(b)
+
+        similarity = []
+
+        for i in range(len(groupA)):
+
+            try:
+                groupB.index(groupA[i])
+                similarity.append(groupA[i])
+            except:
+                continue
+
+        return len(similarity)/max(len(groupA), len(groupB))   
+    
+    def correction(self, word):
+
+        cur = self.root
+        
+        required_sim_ratio = 0.3
+
+        max_sim = 0.0
+
+        sim_words = []
+
+        collected_sim_words = self.collect_words(cur.child[word[0]], word[0])
+        for w in collected_sim_words:
+
+            temp_sim = self.similarity_ratio(word, w)
+
+            if temp_sim > max_sim or temp_sim > required_sim_ratio:
+                # print(f"The sim ratio of {word} when w={w} is {temp_sim}")
+
+                max_sim = temp_sim
+                sim_words.append(w)
+
+        return sim_words if max_sim > required_sim_ratio else word      
+
--- operation.py
+++ operation.py
@@ -2,12 +2,15 @@ from autoCorrect import AutoCorrect
 
 autocorrect = AutoCorrect()
 
-autocorrect.insert("apple")
-autocorrect.insert("app")
-autocorrect.insert("acid")
-autocorrect.insert("aerospace")
-autocorrect.insert("acceptance")
+with open('words.txt', 'r') as file:
 
+    print('Initilizing ...')
+
+    for line in file:
+        autocorrect.insert(line.split()[0])
+
+    print('Initilization Completed!')
+    
 word = input("Enter a word: ")
 
 a = autocorrect.search(word)
